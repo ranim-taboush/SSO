08c61b74009667d6e996ce32935515b3ea54a78d
Ranim Taboush <ranim.taboush@gmail.com>
2024-09-11 16:26:47 +0300

    script nex/script

--- first/app/layout.js
+++ first/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-        <script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>
+        <Script src="https://js.pusher.com/8.2.0/pusher.min.js" />
+        <Script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js" />
         {/* npm install @pusher/push-notifications-web */}
       </head>
-      <body className={inter.className}>{children}</body>
-    </html>
+        <body className={inter.className}>
+          {children}
+          {/* <NextScript /> */}
+        </body>
+      </html>
   );
 }
